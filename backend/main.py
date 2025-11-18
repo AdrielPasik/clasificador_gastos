bc4c8f757839b03fb7b25b0aebbefc70a0840e40
Maximo Bianchi Canelo <63102600+MassiB09@users.noreply.github.com>
2025-11-18 14:26:47 -0300

    Fix indentation for main block in main.py

--- backend/main.py
+++ backend/main.py
@@ -16,5 +16,5 @@ app = FastAPI(title='OCR API')
 app.include_router(api_router, prefix='/api')
 
 
-#if __name__ == '__main__':
-uvicorn.run(app, host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
